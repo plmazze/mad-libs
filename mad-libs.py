ac35d9b65f29e532a9ee842630b072237bc630e5
Paolo <plmazze@gmail.com>
2023-10-27 16:08:21 -0400

    installed nltk package

--- mad-libs.py
+++ mad-libs.py
@@ -1,16 +1,14 @@
 import random
-# import nltk
+import nltk
 
-# print(nltk.__version__)
-
-# nltk.download('all')
+nltk.download('all')
 
 
 when = ['A few decades ago', 'Tomorrow', 'Last evening', 'Recently','On the 30th of May']
 who = ['the tiger', 'the skunk', 'the raccoon', 'the alligator','the wolverine']
 residence = ['box','straw house', 'condo', 'farm', 'townhouse']
 went = ['Theater', 'College','Kingdom', 'school', 'clean the toilet']
-happened = ['made new friends','Eats pasta ', 'found a treasure chest', 'solved a mistery', 'wrote a comic']
+happened = ['made new friends','Eats pasta ', 'found a treasure chest', 'solved a mystery', 'wrote a comic']
 
 noun = input('enter a noun ')
 adjective = input('enter a adjective ')
@@ -22,5 +20,3 @@ conjunction = input('enter a conjunction ')
 print(f'{random.choice(when)}, {noun} {random.choice(who)} lived in a {adjective} {random.choice(residence)}, {pronoun} {adverb} {verb} to the {random.choice(went)}, {conjunction} {random.choice(happened)}')
 
 
-# if !noun:
-#   print('please type a noun')
