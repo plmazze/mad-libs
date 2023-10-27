0608922e1834e259757e8ff9930a055e32941553
Paolo <plmazze@gmail.com>
2023-10-27 14:37:10 -0400

    adding input variables and story text

--- mad-libs.py
+++ mad-libs.py
@@ -7,22 +7,20 @@ import random
 
 
 when = ['A few decades ago', 'Tomorrow', 'Last evening', 'Recently','On the 30th of May']
-who = ['a tiger', 'an skunk', 'a raccoon', 'a alligator','a wolverine']
-name = ['Morgan', 'Mordecai','Jacob', 'Salvador', 'Cal']
-residence = ['Manchester','Parma', 'Germany', 'Greece', 'France']
+who = ['the tiger', 'the skunk', 'the raccoon', 'the alligator','the wolverine']
+residence = ['box','straw house', 'condo', 'farm', 'townhouse']
 went = ['Theater', 'College','Kingdom', 'school', 'clean the toilet']
 happened = ['made new friends','Eats pasta ', 'found a treasure chest', 'solved a mistery', 'wrote a comic']
 
-print(f'{random.choice(when)}, {random.choice(who)} that lived in {random.choice(residence)} went to the {random.choice(went)}, {random.choice(happened)}')
+noun = input('enter a noun ')
+adjective = input('enter a adjective ')
+pronoun = input('enter a pronoun ')
+adverb = input('enter a adverb ')
+verb = input('enter a verb ')
+conjunction = input('enter a conjunction ')
 
-# noun = input('enter a noun')
-# pronoun = input('enter a noun')
-# adjective = input('enter a noun')
-# preposition = input('enter a noun')
-# interjection = input('enter a noun')
-# conjunction = input('enter a noun')
-# verb = input('enter a noun')
-# adverb = input('enter a noun')
+print(f'{random.choice(when)}, {noun} {random.choice(who)} lived in a {adjective} {random.choice(residence)}, {pronoun} {adverb} {verb} to the {random.choice(went)}, {conjunction} {random.choice(happened)}')
+
 
 # if !noun:
 #   print('please type a noun')
